ca81ee8851b6ba6342b49cf10018181388ae502a
Tyler Senne <tsenne2@huskers.unl.edu>
2019-10-13 20:50:26 -0500

    Separate camera from player and create follow script

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Vector3 offset;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
