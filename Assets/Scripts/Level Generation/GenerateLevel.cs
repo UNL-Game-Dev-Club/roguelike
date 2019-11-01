2485b056c97e96c26e3bbf63ced233cbfc04d566
Tyler Senne <tsenne2@huskers.unl.edu>
2019-10-31 21:44:38 -0500

    Created basic skeleton for proc gen script

---
+++ Assets/Scripts/Level Generation/GenerateLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public string path = "";
+
+    public Object[] rooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rooms = Resources.LoadAll(path, typeof(GameObject));
+        
+        foreach (Object room in rooms)
+        {
+            Debug.Log(room.name);
+        }
+    }
+}
---
+++ Assets/Scripts/Level Generation/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public bool top = false;
+    public bool bottom = false;
+    public bool left = false;
+    public bool right = false;
+
+    public int depth = 0;
+}
