2899045ef21f168e5e77789c927fb0310c797c7c
Tyler Senne <tsenne2@huskers.unl.edu>
2019-11-17 21:17:11 -0600

    Super basic proc gen added

--- Assets/Scripts/Level Generation/GenerateLevel.cs
+++ Assets/Scripts/Level Generation/GenerateLevel.cs
@@ -7,14 +7,33 @@ public class GenerateLevel : MonoBehaviour
     public string path = "";
 
     public Object[] rooms;
-    // Start is called before the first frame update
+
+    public GameObject grid;
+
     void Start()
     {
+        grid = GameObject.Find("Grid");
+
         rooms = Resources.LoadAll(path, typeof(GameObject));
+        List<GameObject> level = new List<GameObject>();
         
         foreach (Object room in rooms)
         {
             Debug.Log(room.name);
+        }
+
+        GameObject room1 = Instantiate(rooms[0]) as GameObject;
+        level.Add(room1);
+        room1.transform.parent = grid.transform;
+
+        for (int i = 0; i < 3; i++)
+        {
+            Vector2 pos = level[i].transform.Find("Right").transform.position;
+            GameObject room = Instantiate(rooms[0]) as GameObject;
+            room.transform.parent = grid.transform;
+            Vector3 offset = room.transform.position - room.transform.Find("Left").transform.position;
+            room.transform.position += offset;
+            level.Add(room);
         }
     }
 }
