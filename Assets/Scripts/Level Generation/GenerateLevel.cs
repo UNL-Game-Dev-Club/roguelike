11399e81778934f09d80e1d1fa80fa8bfbdf43bb
Tyler Senne <tsenne2@huskers.unl.edu>
2020-01-08 20:43:59 -0600

    Overwrote existing code with more modular proc gen framework

--- Assets/Scripts/Level Generation/GenerateLevel.cs
+++ Assets/Scripts/Level Generation/GenerateLevel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GenerateLevel : MonoBehaviour
 {
     public string path = "";
+    public int numRooms;
 
     public Object[] rooms;
 
@@ -15,25 +16,53 @@ public class GenerateLevel : MonoBehaviour
         grid = GameObject.Find("Grid");
 
         rooms = Resources.LoadAll(path, typeof(GameObject));
-        List<GameObject> level = new List<GameObject>();
-        
+        List<Room> level = new List<Room>();
+
+        Room room1 = ((GameObject)Instantiate(rooms[0])).GetComponent<Room>();
+        level.Add(room1);
+
+        level.Add(PlaceRoom(rooms, room1, Opening.RIGHT));
+        level.Add(PlaceRoom(rooms, room1, Opening.LEFT));
+
+        //TODO: Actually make this automagic
+    }
+
+    private Room PlaceRoom(Object[] rooms, Room parentRoom, Opening opening)
+    {
+        List<Room> candidates = new List<Room>();
         foreach (Object room in rooms)
         {
-            Debug.Log(room.name);
+            Room candidate = ((GameObject)room).GetComponent<Room>();
+            //TODO: More advanced selection logic
+            if (candidate.HasOpening(OppositeOpening(opening)))
+            {
+                candidates.Add(candidate);
+            }
         }
 
-        GameObject room1 = Instantiate(rooms[0]) as GameObject;
-        level.Add(room1);
-        room1.transform.parent = grid.transform;
+        Room finalRoom = candidates[(int)Random.Range(0f, candidates.Count - 1)];
+        finalRoom = Instantiate(finalRoom);
 
-        for (int i = 0; i < 3; i++)
+        Vector3 roomOffset = finalRoom.transform.position - finalRoom.GetPos(OppositeOpening(opening));
+        finalRoom.transform.position = parentRoom.GetPos(opening) + roomOffset;
+
+        return finalRoom;
+    }
+
+    private Opening OppositeOpening(Opening opening)
+    {
+        switch (opening)
         {
-            Vector2 pos = level[i].transform.Find("Right").transform.position;
-            GameObject room = Instantiate(rooms[0]) as GameObject;
-            room.transform.parent = grid.transform;
-            Vector3 offset = room.transform.position - room.transform.Find("Left").transform.position;
-            room.transform.position += offset;
-            level.Add(room);
+            case Opening.TOP:
+                return Opening.BOTTOM;
+            case Opening.BOTTOM:
+                return Opening.TOP;
+            case Opening.LEFT:
+                return Opening.RIGHT;
+            case Opening.RIGHT:
+                return Opening.LEFT;
+            default:
+                return Opening.BOTTOM;
         }
     }
 }
--- Assets/Scripts/Level Generation/Room.cs
+++ Assets/Scripts/Level Generation/Room.cs
@@ -4,10 +4,93 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public bool top = false;
-    public bool bottom = false;
-    public bool left = false;
-    public bool right = false;
+    public bool hasTop = false;
+    public bool hasBottom = false;
+    public bool hasLeft = false;
+    public bool hasRight = false;
+
+    public bool HasOpening(Opening opening)
+    {
+        switch (opening)
+        {
+            case Opening.TOP:
+                if (hasTop)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case Opening.BOTTOM:
+                if (hasBottom)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case Opening.LEFT:
+                if (hasLeft)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            case Opening.RIGHT:
+                if (hasRight)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            default:
+                return false;
+        }
+    }
+    public Vector3 GetPos(Opening opening)
+    {
+        switch (opening)
+        {
+            case Opening.TOP:
+                return topPos;
+            case Opening.BOTTOM:
+                return bottomPos;
+            case Opening.LEFT:
+                return leftPos;
+            case Opening.RIGHT:
+                return rightPos;
+            default:
+                return Vector3.zero;
+        }
+    }
+
+    public Vector3 rightPos
+    {
+        get { return transform.Find("Right").gameObject.transform.position; }
+    }
+    public Vector3 leftPos
+    {
+        get { return transform.Find("Left").gameObject.transform.position; }
+    }
+    public Vector3 topPos
+    {
+        get { return transform.Find("Top").gameObject.transform.position; }
+    }
+    public Vector3 bottomPos
+    {
+        get { return transform.Find("Bottom").gameObject.transform.position; }
+    }
 
     public int depth = 0;
+}
+
+public enum Opening
+{
+    TOP, LEFT, RIGHT, BOTTOM
 }
